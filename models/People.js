89e7692b2bc62b86710ed65a5639710cc5fea150
Arifatul Ronansyah <v.fatul@gmail.com>
2019-02-27 21:44:28 +0700

    Init galaxi app API

---
+++ models/People.js
@@ -0,0 +1,20 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * People Model
+ * ==========
+ */
+var People = new keystone.List('People');
+
+People.add({
+	name: { type: Types.Name, required: true },
+	height: { type: Types.Number, required: false, initial: false },
+	mass: { type: Types.Number, required: false, initial: false },
+	gender: { type: String },
+});
+
+/**
+ * Registration
+ */
+People.register();
